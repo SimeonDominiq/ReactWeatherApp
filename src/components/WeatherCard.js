6622da027c55b6295a397a8d2a9e1eca5426ecd4
Simeon Dominiq <simeondominiq@gmail.com>
2019-01-24 12:28:14 +0100

    Seperation of Concerns views & logics

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,34 +1,20 @@
 import React, { Component } from "react";
-import temp_min_svg from "../images/temp_min_svg.svg";
-import temp_max_svg from "../images/temp_max_svg.svg";
+import WeatherView from "./WeatherView";
+import sampleData from "../json_test_data/sampleWeatherData";
 
 class WeatherCard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      weatherData: sampleData
+    };
+  }
+
   render() {
-    return (
-      <div className="weather_card">
-        <span className="city_name_text">London</span>
-        <div className="weather_icon_container">Icon</div>
-        <div className="temperature_text_container">
-          <span className="temperature_text">23</span>
-          <span className="temperature_metric_text">°</span>
-          <span className="temp_weather_condition">Sunny</span>
-        </div>
-
-        <div className="temp_min_max_container">
-          <div className="min_container">
-            <img src={temp_min_svg} alt="Min Svg" />
-            <span className="min_temp_text">12</span>
-            <span className="min_text">Min</span>
-          </div>
-
-          <div className="max_container">
-            <img src={temp_max_svg} alt="Max Svg" />
-            <span className="max_temp_text">12</span>
-            <span className="max_text">Max</span>
-          </div>
-        </div>
-      </div>
-    );
+    const WeatherComponent = this.state.weatherData.map(data => {
+      return <WeatherView key={data.id} weatherdata={data} />;
+    });
+    return <div className="main_container">{WeatherComponent}</div>;
   }
 }
 
---
+++ src/json_test_data/sampleWeatherData.js
@@ -0,0 +1,15 @@
+const weatherData = [
+  {
+    id: 1,
+    name: "Pencil",
+    price: "100",
+    description: "This is a pencil"
+  },
+  {
+    id: 2,
+    name: "Biro",
+    price: "50",
+    description: "This is a Biro"
+  }
+];
+export default weatherData;
